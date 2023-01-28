acd134728c9c8e751222452c165b71d99dba179f
Alesandro Zuzic <azuzic@student.unipu.hr>
2023-01-28 20:37:03 +0100

    Added SceneBuilder > Design updated     - added game icons     - added game carousel     - added game titles     - added game ratings     - responsiveness adjusted

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.skin.ButtonSkin;
+import javafx.util.Duration;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+
 }
+
